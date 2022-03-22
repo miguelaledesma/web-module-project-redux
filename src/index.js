fe93772861e3a3fd3a63f676dfccdb1934421b21
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-22 13:53:43 -0700

    task 1 and 2 are completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import reducer from './reducers';
@@ -9,9 +9,14 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers())
+
 ReactDOM.render(
   <Router>
+    <Provider store = {store}>
       <App />
+      </Provider>
   </Router>,
   document.getElementById('root')
 );
